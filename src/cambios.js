0eee7bec52ea831369b3309ae0882dd9c47e645b
TheLaucha <47804695+TheLaucha@users.noreply.github.com>
2020-11-06 11:30:21 -0300

    Delete cambios.js

--- src/cambios.js
+++
@@ -1,9 +0,0 @@
-// OBTENER POKEMONES
-
-const $URL = "https://pokeapi.co/api/v2/pokemon"
-
-export async function obtenerPokemones(pokemon){
-    return fetch(`${$URL}${pokemon}`)
-    .then(respuesta => respuesta.json())
-    .then(respuestaJSON => respuestaJSON)
-}
